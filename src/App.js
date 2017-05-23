25aa852c17fa63a79f04a66f3f1e7e8370fd2ebf
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-05-24 09:21:13 +1000

    insert router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import React, { Component } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+
 import axios from 'axios';
 import './App.css';
 
@@ -28,6 +36,14 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 // to the correct localizer.
 BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
 
+var CalendarWrapper = React.createClass({
+render: function () {
+  return (
+    <BigCalendar events={this.state.events} />
+  );
+}
+});
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -52,6 +68,8 @@ class App extends Component {
       });
   };
 
+
+
   render() {
     var layout = [
       { i: 'a', x: 0, y: 0, w: 12, h: 12},
@@ -62,24 +80,29 @@ class App extends Component {
     ];
 
     return (
+      <Router>
       <Row className="noMargin">
-        <Col s={11} className='grid-example'>
-          <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1250}>
-            <div key={'a'}><Greeting /></div>
-            <div key={'b'}><Quote quoteData={this.state.allData.quoteText}/></div>
-             {/* <div key={'c'} className="calendar"><BigCalendar events={this.state.events} /></div> */}
-             {/* <div key={'d'}><News /></div> */}
-          </ReactGridLayout>
-        </Col>
-        <Col s={1} className='grid-example sidebar'>
-          <div className="sideBarItem sideBarImage"><Forecast/></div>
-          <div className="sideBarItem sideBarItemFirst sideBarImage centreAlign"><img src="https://maxcdn.icons8.com/Share/icon/p1em/Very_Basic//home1600.png" height="70px"/></div>
-          <div className="sideBarItem sideBarImage centreAlign"><img src="http://res.cloudinary.com/dzctpgu9d/image/upload/v1495531800/calendar-512_d4wava.png" height="70px"/></div>
-          <div className="sideBarItem sideBarImage centreAlign"><img src="https://cdn3.iconfinder.com/data/icons/basic-mobile-part-2/512/newspaper-512.png" height="70px"/></div>
-          <div className="sideBarItem sideBarImage centreAlign"><img src="http://res.cloudinary.com/dzctpgu9d/image/upload/v1495531618/0106-currencies-512_ezvz0j.png" height="70px"/></div>
-          <div className="sideBarItem sideBarImage centreAlign"><CurrencyExchange /></div>
-        </Col>
+
+      <Col s={11} className='grid-example'>
+        <Route exact path="/" component={Greeting}/>
+        <Route path="/news" component={News}/>
+        <Route path="/calendar" component={() => (<BigCalendar events={this.state.events} />)}/>
+        <div key={'b'}><Quote quoteData={this.state.allData.quoteText}/></div>
+          {/* <div key={'c'} className="calendar"><BigCalendar events={this.state.events} /></div> */}
+          {/* <div key={'d'}><News /></div> */}
+      </Col>
+      <Col s={1} className='grid-example sidebar'>
+            <div className="sideBarItem sideBarImage"><Forecast/></div>
+            <div className="sideBarItem sideBarItemFirst sideBarImage centreAlign"><Link to="/"><img src="https://maxcdn.icons8.com/Share/icon/p1em/Very_Basic//home1600.png" height="70px"/></Link></div>
+            <div className="sideBarItem sideBarImage centreAlign"><Link to="/calendar"><img src="http://res.cloudinary.com/dzctpgu9d/image/upload/v1495531800/calendar-512_d4wava.png" height="70px"/></Link></div>
+            <div className="sideBarItem sideBarImage centreAlign"><Link to="/news"><img src="https://cdn3.iconfinder.com/data/icons/basic-mobile-part-2/512/newspaper-512.png" height="70px"/></Link></div>
+            <div className="sideBarItem sideBarImage centreAlign"><img src="http://res.cloudinary.com/dzctpgu9d/image/upload/v1495531618/0106-currencies-512_ezvz0j.png" height="70px"/></div>
+            <div className="sideBarItem sideBarImage centreAlign"><CurrencyExchange /></div>
+
+      </Col>
+
       </Row>
+      </Router>
     );
   };
 };
