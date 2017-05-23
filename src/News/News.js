2df19abb9c306b5f3da3137bbe966715bad2f19d
Kirsten Frager <kirstenfrager@kirstens-air.ait-sydney.net.au>
2017-05-23 15:32:47 +1000

    conflict resolved

--- src/News/News.js
+++ src/News/News.js
@@ -34,29 +34,24 @@ class News extends Component {
   render() {
     return (
       <div className="row">
-
-            {this.state.allData.map((data, i) =>
-              <MuiThemeProvider>
-                <div className="col s12 m4">
-                <div className="card">
-                    <div>
-                      <div className="card-image">
-                        <img src={data.urlToImage}/>
-                        <span className="card-title">{data.title}</span>
-                      </div>
-                      <div className="card-content">
-                        <p>{data.description}</p>
-                      </div>
-                      <div className="card-action">
-                        <a href={data.url}>Read Full Article</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </MuiThemeProvider>
-
-          )}
-
+        {this.state.allData.map((data, i) =>
+          <div className="col s6 m3">
+          <div className="card">
+            <div>
+              <div className="card-image">
+                <img src={data.urlToImage}/>
+                <span className="card-title">{data.title}</span>
+              </div>
+              <div className="card-content">
+                <p>{data.description}</p>
+              </div>
+              <div className="card-action">
+                <a href={data.url}>Read Full Article</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
     );
   };
