0640b670e100527889aecd87996e027ce1f1a40a
Kirsten Frager <kirstenfrager@kirstens-air.ait-sydney.net.au>
2017-05-23 20:06:42 +1000

    homepage styling

--- src/App.js
+++ src/App.js
@@ -54,9 +54,9 @@ class App extends Component {
 
   render() {
     var layout = [
-      { i: 'a', x: 0, y: 0, w: 12, h: 4},
-      { i: 'b', x: 0, y: 2, w: 3, h: 10, minW: 2, maxW: 4 },
-      { i: 'c', x: 0, y: 4, w: 12, h: 20 },
+      { i: 'a', x: 0, y: 0, w: 8, h: 4},
+      { i: 'b', x: 0, y: 2, w: 10, h: 5, minW: 2, maxW: 4 },
+      { i: 'c', x: 0, y: 4, w: 3, h: 8 },
       { i: 'd', x: 0, y: 4, w: 12, h: 20 },
       { i: 'e', x: 0, y: 4, w: 5, h: 8 },
     ];
@@ -64,10 +64,10 @@ class App extends Component {
     return (
       <Row>
         <Col s={11} className='grid-example'>
-          <Greeting />
-          <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1400}>
-            <div key={'a'}><Quote quoteData={this.state.allData.quoteText}/></div>
-             <div key={'b'}><Forecast /></div>
+          <ReactGridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+            <div key={'a'} className="greeting"><Greeting /></div>
+            <div key={'b'}><Quote quoteData={this.state.allData.quoteText}/></div>
+            <div key={'c'}><Forecast /></div>
              {/* <div key={'c'}><BigCalendar events={this.state.events} /></div> */}
              {/* <div key={'d'}><News /></div> */}
              {/* <div key={'e'}><CurrencyExchange /></div> */}
