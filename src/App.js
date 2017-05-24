2551cba3eb6c4e72566fea1f5442b643fec22928
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-05-24 10:08:30 +1000

    update styling

--- src/App.js
+++ src/App.js
@@ -36,20 +36,81 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 // to the correct localizer.
 BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
 
-var CalendarWrapper = React.createClass({
-render: function () {
-  return (
-    <BigCalendar events={this.state.events} />
-  );
-}
-});
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       allData: [],
-      events: [],
+      events: [
+  {
+    'title': 'All Day Event',
+    'allDay': true,
+    'start': new Date(2017, 5, 0),
+    'end': new Date(2017, 5, 1)
+  },
+  {
+    'title': 'Long Event',
+    'start': new Date(2017, 5, 7),
+    'end': new Date(2017, 5, 10)
+  },
+
+  {
+    'title': 'DTS STARTS',
+    'start': new Date(2017, 5, 13, 0, 0, 0),
+    'end': new Date(2017, 5, 20, 0, 0, 0)
+  },
+
+  {
+    'title': 'DTS ENDS',
+    'start': new Date(2017, 10, 6, 0, 0, 0),
+    'end': new Date(2017, 10, 13, 0, 0, 0)
+  },
+
+  {
+    'title': 'Some Event',
+    'start': new Date(2017, 5, 9, 0, 0, 0),
+    'end': new Date(2017, 5, 9, 0, 0, 0)
+  },
+  {
+    'title': 'Conference',
+    'start': new Date(2017, 5, 11),
+    'end': new Date(2017, 5, 13),
+    desc: 'Big conference for important people'
+  },
+  {
+    'title': 'Meeting',
+    'start': new Date(2017, 5, 12, 10, 30, 0, 0),
+    'end': new Date(2017, 5, 12, 12, 30, 0, 0),
+    desc: 'Pre-meeting meeting, to prepare for the meeting'
+  },
+  {
+    'title': 'Lunch',
+    'start':new Date(2017, 5, 12, 12, 0, 0, 0),
+    'end': new Date(2017, 5, 12, 13, 0, 0, 0),
+    desc: 'Power lunch'
+  },
+  {
+    'title': 'Meeting',
+    'start':new Date(2017, 5, 12,14, 0, 0, 0),
+    'end': new Date(2017, 5, 12,15, 0, 0, 0)
+  },
+  {
+    'title': 'Happy Hour',
+    'start':new Date(2017, 5, 12, 17, 0, 0, 0),
+    'end': new Date(2017, 5, 12, 17, 30, 0, 0),
+    desc: 'Most important meal of the day'
+  },
+  {
+    'title': 'Dinner',
+    'start':new Date(2017, 5, 12, 20, 0, 0, 0),
+    'end': new Date(2017, 5, 12, 21, 0, 0, 0)
+  },
+  {
+    'title': 'Birthday Party',
+    'start':new Date(2017, 5, 13, 7, 0, 0),
+    'end': new Date(2017, 5, 13, 10, 30, 0)
+  }
+],
     };
   };
 
@@ -81,29 +142,28 @@ class App extends Component {
 
     return (
       <Router>
-      <Row className="noMargin">
+        <Row className="noMargin">
+          <Col s={11} className='grid-example'>
 
-      <Col s={11} className='grid-example'>
-        <div className="fullHeight">
-          <Route exact path="/" component={Greeting}/>
-          <Route path="/news" component={News}/>
-          <Route path="/calendar" component={() => (<BigCalendar events={this.state.events} />)}/>
-          {/* <div key={'b'}><Quote quoteData={this.state.allData.quoteText}/></div> */}
-          {/* <div key={'c'} className="calendar"><BigCalendar events={this.state.events} /></div> */}
-          {/* <div key={'d'}><News /></div> */}
-        </div>
-      </Col>
-      <Col s={1} className='grid-example sidebar'>
+              <Route exact path="/" component={Greeting}/>
+              <Route path="/news" component={News}/>
+              <div className="fullHeight">
+              <Route path="/calendar" component={() => (<BigCalendar events={this.state.events} />)}/>
+              </div>
+              {/* <div key={'b'}><Quote quoteData={this.state.allData.quoteText}/></div> */}
+              {/* <div key={'c'} className="calendar"><BigCalendar events={this.state.events} /></div> */}
+              {/* <div key={'d'}><News /></div> */}
+
+          </Col>
+          <Col s={1} className='grid-example sidebar'>
             <div className="sideBarItem sideBarImage"><Forecast/></div>
             <div className="sideBarItem sideBarItemFirst sideBarImage centreAlign"><Link to="/"><img src="https://maxcdn.icons8.com/Share/icon/p1em/Very_Basic//home1600.png" height="70px"/></Link></div>
             <div className="sideBarItem sideBarImage centreAlign"><Link to="/calendar"><img src="http://res.cloudinary.com/dzctpgu9d/image/upload/v1495531800/calendar-512_d4wava.png" height="70px"/></Link></div>
             <div className="sideBarItem sideBarImage centreAlign"><Link to="/news"><img src="https://cdn3.iconfinder.com/data/icons/basic-mobile-part-2/512/newspaper-512.png" height="70px"/></Link></div>
             <div className="sideBarItem sideBarImage centreAlign"><img src="http://res.cloudinary.com/dzctpgu9d/image/upload/v1495531618/0106-currencies-512_ezvz0j.png" height="70px"/></div>
             <div className="sideBarItem sideBarImage centreAlign"><CurrencyExchange /></div>
-
-      </Col>
-
-      </Row>
+          </Col>
+        </Row>
       </Router>
     );
   };
